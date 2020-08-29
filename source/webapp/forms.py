34547ddfe9f1efbb9f8fff3d4aadff7e5c0de549
violettalivada <vilkalivada@gmail.com>
2020-08-29 10:12:29 +0300

    Созданы формы для вопроса и варианта ответа

---
+++ source/webapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import *
+
+
+class PollForm(forms.ModelForm):
+    class Meta:
+        model = Poll
+        fields = ['question']
+
+
+class ChoiceForm(forms.ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['choice_text']
